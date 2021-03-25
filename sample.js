328099fe387585c7716f4d365cfc4a47f60496bd
OseungKwon <54261116+OseungKwon@users.noreply.github.com>
2021-03-25 14:52:38 +0900

    add

--- sample.js
+++ sample.js
@@ -1,2 +1,3 @@
-const sequences = (length = 0) => Array.from({length},(_, i)=> i);
-console.log(sequences(3));
+const arr = ['hello', 'world'];
+arr.map(x=> x.split('')).flat();
+arr.flatMap(x=>x.split(''));
